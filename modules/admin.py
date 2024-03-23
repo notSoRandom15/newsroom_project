62ba3e48209f2db0a40944055d5e5d60c441055c
Demetre <deme.barbakadze2@gmail.com>
2024-03-23 11:32:23 +0400

    registered Block model

--- modules/admin.py
+++ modules/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from adminsortable2.admin import SortableAdminMixin
 
-from .models import Menu
+from .models import Menu, Block
 
 @admin.register(Menu)
 class MenuAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = ['name', "is_external", "is_active", "my_order"]
+
+admin.site.register(Block)
